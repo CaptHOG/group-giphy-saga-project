6e57305978abc79ef9c5fc9391408360c55345f2
Jeremy Hanson <rockbasser88@hotmail.com>
2023-01-26 15:52:40 -0600

    setup routes in favorite.router and made Favorites and Search components

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,27 +1,73 @@
 const express = require('express');
 const pool = require('../modules/pool');
-
+const giphy_api_key = process.env.GIPHY_API_KEY;
 const router = express.Router();
 
-// return all favorite images
+
+// GET - return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  pool.query('SELECT * FROM "favorites";')
+    .then((result) => {
+      console.log(result.rows);
+      res.send(result.rows);
+    }).catch((error) => {
+      console.error('Error GET /api/favorite', error);
+      res.sendStatus(500);  
+    });
 });
 
-// add a new favorite
+// POST - add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  const sqlText = `
+    INSERT INTO "favorites" ("url", "category_id")
+    VALUES ($1, $2)
+  `;
+  const sqlValues = [req.body.url, req.body.category_id];
+
+  pool.query(sqlText, sqlValues)
+    .then((result) => {
+      console.log(`Added image into favorites`);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.error('Error POST /api/favorites', error);
+      res.sendStatus(500); 
+    })
 });
 
 // update given favorite with a category id
+// req.body should contain a category_id to add to this favorite image
 router.put('/:favId', (req, res) => {
-  // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
+  let idToUpdate = req.params.id
+  let newCategory = req.body.category_id
+  let sqlValues = [newCategory, idToUpdate]
+
+  let sqlQuery = `
+  UPDATE "favorites"
+    SET "category_id" = $1
+    WHERE "id" = $2;
+  `;
+
+  pool.query(sqlQuery, sqlValues)
+    .then((dbRes)=>{
+      console.log('successful update from PUT /api/favorites', dbRes);
+      res.sendStatus(201)
+    }).catch(( dbErr)=>{
+      console.error('Error PUT /api/favorites', dbErr);
+      res.sendStatus(500)
+    })
 });
 
-// delete a favorite
+// DELETE - delete a favorite
 router.delete('/', (req, res) => {
-  res.sendStatus(200);
+  pool.query(`DELETE FROM "favorites" WHERE id=$1`, [req.params.id] )
+    .then((result) => {
+    res.sendStatus(200);
+  }).catch((error) => {
+    console.log('Error DELETE /api/favorites', error);
+    res.sendStatus(500);
+  })
 });
+
 
 module.exports = router;

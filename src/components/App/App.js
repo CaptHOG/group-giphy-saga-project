cd0bab5964cb5b53218cc8337961d466cdb36c85
Connor Clark <connordeclark@gmail.com>
2023-01-26 16:06:43 -0600

    Whoops, did this on main, index updated with some SAGA root

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import {useDispatch,useSelector} from 'react-redux'
 function App(props) {
 
 
- const getFavorite(){
+ const getFavorite = () => {
 const dispatch = useDispatch();
 //const variable-that-takes-form-of-the-store = useSelector((store)=>store.reducerName) 
   //look for Connor get message

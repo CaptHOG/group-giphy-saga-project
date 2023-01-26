ce62bcf6350a2c9e71b610aab8d064cf3f39a325
Tou Fue Yang <touyangbiz@gmail.com>
2023-01-26 15:55:18 -0600

    save what I have

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {useDispatch,useSelector} from 'react-redux'
+
 
 function App(props) {
+
+
+ const getFavorite(){
+const dispatch = useDispatch();
+//const variable-that-takes-form-of-the-store = useSelector((store)=>store.reducerName) 
+  //look for Connor get message
+  dispatch({
+    type:'SAGA_FETCH_GIFS'
+  })
+ }
+
+
+
   return (
     <div>
       <h1>Giphy Search!</h1>
+      
     </div>
   );
 }
